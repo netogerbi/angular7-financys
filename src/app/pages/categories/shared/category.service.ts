d8e71051e756b4bd8264e3b70e8fbc9b85c2df94
Trix <sistema@trixsys.com.br>
2019-02-06 13:44:55 -0200

    Adding http intercept using in-memory-web-api (error)

--- src/app/pages/categories/shared/category.service.ts
+++ src/app/pages/categories/shared/category.service.ts
@@ -3,8 +3,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { map, catchError, flatMap } from 'rxjs/operators';
-import { httpClientInMemBackendServiceFactory } from 'angular-in-memory-web-api';
-import { url } from 'inspector';
 
 
 @Injectable({
