d9e82a0ae32ca8f05930acd27702eec4491e60c5
Trix <sistema@trixsys.com.br>
2019-02-21 12:36:07 -0300

    Entry list not working

--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -1,13 +1,14 @@
+import { EntryListComponent } from './entry-list/entry-list.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EntriesRoutingModule } from './entries-routing.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [EntryListComponent],
   imports: [
     CommonModule,
-    EntriesRoutingModule
+    EntriesRoutingModule,
   ]
 })
 export class EntriesModule { }
---
+++ src/app/pages/entries/entry-list/entry-list.component.ts
@@ -0,0 +1,33 @@
+import { Entry } from './../shared/entry.model';
+import { EntryService } from './../shared/entry.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-entry-list',
+  templateUrl: './entry-list.component.html',
+  styleUrls: ['./entry-list.component.sass']
+})
+export class EntryListComponent implements OnInit {
+
+  entries: Entry[] = [];
+
+  constructor(private entryService: EntryService) { }
+
+  ngOnInit() {
+    this.entryService.getAll().subscribe(
+      entries => this.entries = entries,
+      error => alert('Erro ao carregar a lista' + error),
+    );
+  }
+
+  deleteEntry(entry: Entry) {
+    const mustDelete = confirm('Deseja realmente excluir este item?');
+    if (mustDelete) {
+      this.entryService.delete(entry.id).subscribe(
+        () => this.entries = this.entries.filter(element => element !== entry),
+        () => alert('Erro na exclusão'),
+      );
+    }
+  }
+
+}
