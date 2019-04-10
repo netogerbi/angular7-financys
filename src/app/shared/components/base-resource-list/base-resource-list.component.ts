01b744ce1db6d07946440ab1145c8b0b938b2176
Reculos Gerbi Neto <neto@savy.com.br>
2019-04-10 12:42:41 -0300

    adding BaseResourceListComponent

---
+++ src/app/shared/components/base-resource-list/base-resource-list.component.ts
@@ -0,0 +1,30 @@
+import { BaseResourceService } from 'src/app/shared/services/base-resource.service';
+import { BaseResourceModel } from 'src/app/shared/models/base-resource.model';;
+
+import { OnInit } from '@angular/core';
+
+
+export class BaseResourceListComponent<T extends BaseResourceModel> implements OnInit {
+
+  resources: T[] = [];
+
+  constructor(private resourceService: BaseResourceService<T>) { }
+
+  ngOnInit() {
+    this.resourceService.getAll().subscribe(
+      resources => this.resources = resources.sort((a, b) => b.id - a.id),
+      error => alert('Erro ao carregar a lista' + error),
+    );
+  }
+
+  deleteEntry(resource: T) {
+    const mustDelete = confirm('Deseja realmente excluir este item?');
+    if (mustDelete) {
+      this.resourceService.delete(resource.id).subscribe(
+        () => this.resources = this.resources.filter(element => element !== resource),
+        () => alert('Erro na exclusão'),
+      );
+    }
+  }
+
+}
