c6bfbeb35d8536d9d426a76ed18cd2bf7b630e9f
Reculos Gerbi Neto <neto@savy.com.br>
2019-04-26 12:38:49 -0300

    adding error form component

---
+++ src/app/shared/components/form-field-error/form-field-error.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-form-field-error',
+  template: `
+    <p class='text-danger'>
+      {{ errorMessage }}
+    </p>
+  `,
+  styleUrls: ['./form-field-error.component.sass']
+})
+export class FormFieldErrorComponent implements OnInit {
+
+
+  @Input('form-control') formControl: FormControl;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public get errorMessage(): string | null {
+    if (this.mustShowErrorMessage()) {
+      return this.getErrorMessage();
+    } else {
+      return null;
+    }
+  }
+
+  private mustShowErrorMessage(): boolean {
+    return this.formControl.invalid && this.formControl.touched;
+  }
+
+  private getErrorMessage(): string | null {
+    if ( this.formControl.errors.required ) {
+     return 'Dado Obrigatório';
+    } else if ( this.formControl.errors.minlength ) {
+      const requiredLenth = this.formControl.errors.minlength.requiredLength;
+      return `Deve ter no mínimo ${requiredLenth} caracteres`;
+    } else if ( this.formControl.errors.maxlength ) {
+      const requiredLength = this.formControl.errors.maxlength.requiredLength;
+      return `Deve ter no máximo ${requiredLength} caracteres`;
+    }
+
+  }
+
+}
