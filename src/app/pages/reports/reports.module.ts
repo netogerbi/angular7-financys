b8a4320e6b3995f58dca1865dcf3553d865c7ad6
Reculos Gerbi Neto <neto@savy.com.br>
2019-05-08 12:57:04 -0300

    creating reports funcionality

--- src/app/pages/reports/reports.module.ts
+++ src/app/pages/reports/reports.module.ts
@@ -4,9 +4,12 @@ import { ReportsRoutingModule } from './reports-routing.module';
 import { ReportsComponent } from './reports/reports.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 
+import { ChartModule } from 'primeng/chart';
+
 @NgModule({
   declarations: [ReportsComponent],
   imports: [
+    ChartModule,
     SharedModule,
     ReportsRoutingModule
   ]
